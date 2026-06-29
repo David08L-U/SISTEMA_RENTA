b16f5ac4aa302150d1c19ee140287268f2aeb404
David08L-U <davidlopez@example.com>
2026-06-28 20:35:02 -0600

    Estructura inicial del proyecto por capas

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace RentaDepartamentosWeb.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
